c36c7cbbd1e6705f65e2ef5f45ff9740c6475701
takemotoooo-eeic <takemoto-kengo2262@g.ecc.u-tokyo.ac.jp>
2025-06-20 13:10:25 +0900

    lint

--- backend/api/models/ddsp/loss/__init__.py
+++ backend/api/models/ddsp/loss/__init__.py
@@ -1,10 +1,9 @@
 from .fd_regularization_loss import FrequencyDomainRegularizationLoss
 from .loss import Loss, LossInputs
-from .mel_loss import MelLoss, mel_loss
+from .mel_loss import MelLoss
 from .td_regularization_loss import TimeDomainRegularizationLoss
 
 __all__ = [
-    "mel_loss",
     "MelLoss",
     "TimeDomainRegularizationLoss",
     "FrequencyDomainRegularizationLoss",
